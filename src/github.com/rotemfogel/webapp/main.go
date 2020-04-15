f40e586975d91a8c850ef720f2ba1c0d4a9f3ae3
Rotem Fogel <rotem@seekingalpha.com>
2020-04-15 16:59:18 +0300

    working with templates

--- src/github.com/rotemfogel/webapp/main.go
+++ src/github.com/rotemfogel/webapp/main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
 func main() {
+	http.Handle("/img", http.FileServer(http.Dir("public")))
+	http.Handle("/css", http.FileServer(http.Dir("public")))
+
+	templates := populateTemplates()
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		f := request.URL.Path[1:]
+		var t *template.Template
+		if strings.HasSuffix(f, "html") {
+			t = templates.Lookup(f)
+		} else {
+			t = templates.Lookup(f + ".html")
+		}
+		if t != nil {
+			err := t.Execute(writer, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			writer.WriteHeader(http.StatusNotFound)
+		}
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+const (
+	basePath = "templates"
+)
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
